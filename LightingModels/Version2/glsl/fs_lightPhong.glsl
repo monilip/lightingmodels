0758bb9be9903b5a6a917907df69dd08b28621af
Monika Lipowska <monilip@onet.eu>
2015-10-13 14:04:31 +0200

    Revert "Revert "Added Strauss model""
    This reverts commit c202d07d6e2062e3f075973e98054ce41dd35875.

--- LightingModels/Version2/glsl/fs_lightPhong.glsl
+++ LightingModels/Version2/glsl/fs_lightPhong.glsl
@@ -17,9 +17,6 @@ uniform vec3 ambientColor;
 uniform vec3 diffuseColor;
 uniform vec3 specularColor;
 
-uniform vec3 Ka;
-uniform vec3 Kd;
-uniform vec3 Ks;
 uniform float Ns;
 
 uniform bool isTexture;
@@ -62,9 +59,9 @@ void main()
 		Dif.b +=f_color.b;
 	}
 	
-	Amb = Amb * Ka;
-	Dif = Dif * diffuse * Kd;
-	Spec = Spec * specular * Ks;
+	Amb = Amb ;//* Ka;
+	Dif = Dif * diffuse ;//* Kd;
+	Spec = Spec * specular;// * Ks;
 
 	outputColor = vec4(Amb + Dif + Spec, 1.0f);
 }
